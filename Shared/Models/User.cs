c38c87798988a2a5cebad617e17c76fcfababbe9
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-23 21:38:05 +0100

    Add Json properties to user class.

--- Shared/Models/User.cs
+++ Shared/Models/User.cs
@@ -1,9 +1,16 @@
+using Newtonsoft.Json;
+
 namespace Shared.Models;
 
 public class User
 {
+    [JsonProperty("username")]
     public string Username { get; set; }
+    
+    [JsonProperty("password")]
     public string Password { get; set; }
+   
+    [JsonProperty("privileges")]
     public Privileges Privileges { get; set; } = Privileges.User;
 
     public User(string username, string password, Privileges privileges)
