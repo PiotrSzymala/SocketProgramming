446d4de769cd4b4b9c5d23d5c88e0064d8ce09e5
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-24 15:45:20 +0100

    UserCreator.cs reformat.

--- Server/UserCreator.cs
+++ Server/UserCreator.cs
@@ -1,6 +1,7 @@
 using System.Net.Sockets;
 using System.Text;
 using Newtonsoft.Json;
+using Shared;
 using Shared.Models;
 
 namespace Server;
@@ -9,63 +10,39 @@ public static class UserCreator
 {
     public static void CreateUser(Socket clientSocket)
     {
-         var message = SendToClient("Set username");
+         var message = DataSender.SendData("Set username");
 
         clientSocket.Send(message);
         
-        var username = GetDataFromClient(clientSocket);
+        var username = DataReceiver.GetData(clientSocket);
 
         if (!File.Exists($"{username}.json"))
         {
-            message = SendToClient("Set password");
+            message = DataSender.SendData("Set password");
             clientSocket.Send(message);
             
-            var password = GetDataFromClient(clientSocket);
+            var password = DataReceiver.GetData(clientSocket);
 
-            message = SendToClient("If you want admin account type in a special password:");
+            message = DataSender.SendData("If you want admin account type in a special password:");
             clientSocket.Send(message);
             
-            var specialPassword = GetDataFromClient(clientSocket);
-
+            var specialPassword = DataReceiver.GetData(clientSocket);
             var privileges = (specialPassword == "root123" ? Privileges.Admin : Privileges.User);
-
             var user = new User(username, password, privileges);
             
-
             using (StreamWriter file = File.CreateText($"{username}.json"))
             {
                 var result = JsonConvert.SerializeObject(user);
                 file.Write(result);
             }
             
-            message = SendToClient("User created!");
+            message = DataSender.SendData("User created!");
             clientSocket.Send(message);
         }
         else
         {
-            message = SendToClient("User already exists!");
+            message = DataSender.SendData("User already exists!");
             clientSocket.Send(message);
         }
-    }
-
-    private static string GetDataFromClient(Socket clientSocket)
-    {
-        var bytesU = new byte[1024];
-        var numByte = clientSocket.Receive(bytesU);
-
-        var received = Encoding.ASCII.GetString(bytesU, 0, numByte);
-        var MessageToClient = JsonConvert.DeserializeObject<MyMessage>(received);
-
-        var result = MessageToClient.Message;
-        return result;
-    }
-
-    private static byte[] SendToClient(string messageContext)
-    {
-        var MessageToClient = new MyMessage();
-        MessageToClient.Message = messageContext;
-        var toSend = JsonConvert.SerializeObject(MessageToClient);
-        var message = Encoding.ASCII.GetBytes(toSend);
-        return message;
     }
 }
