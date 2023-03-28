eda6e8f34497e4b09dfd378f45a9c0245522a8df
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-28 20:07:13 +0200

    ServerVersion changed to 0.0.2

--- Server/ServerInformation.cs
+++ Server/ServerInformation.cs
@@ -2,6 +2,6 @@ namespace Server;
 
 public static class ServerInformation
 {
-    public static string ServerVersion { get; set; } = "0.0.1";
+    public static string ServerVersion { get; set; } = "0.0.2";
     public static DateTime ServerCreationDate { get; set; } = DateTime.Now;
 }
