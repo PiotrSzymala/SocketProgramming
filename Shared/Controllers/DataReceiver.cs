2de080d25b234780dc60a757155cb03ea15d26f6
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-04-07 21:20:46 +0200

    DataReceiver.cs refactor (create constructor and private field ITransferStructure)

--- Shared/Controllers/DataReceiver.cs
+++ Shared/Controllers/DataReceiver.cs
@@ -6,11 +6,18 @@ using Shared.Models;
 namespace Shared.Controllers;
 
 public class DataReceiver : IDataReceiver
+{
+    private ITransferStructure _transferStructure;
+
+    public DataReceiver(ITransferStructure transferStructure)
     {
-        public string GetData(Socket socket)
+        _transferStructure = transferStructure;
+    }
+
+    public string GetData()
         {
             var bytesToReceive = new byte[1024];
-            var numByte = socket.Receive(bytesToReceive);
+            var numByte = _transferStructure.Receive(bytesToReceive);
 
             var receivedString = Encoding.ASCII.GetString(bytesToReceive, 0, numByte);
             var clientServerResponse = JsonConvert.DeserializeObject<CommandFromUser>(receivedString);
