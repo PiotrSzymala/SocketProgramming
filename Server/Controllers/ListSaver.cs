77d41e61f51552dc28a81ab351d3bae20a24132f
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-04-07 21:28:03 +0200

    Provide more detailed directory path to SaveList method.

--- Server/Controllers/ListSaver.cs
+++ Server/Controllers/ListSaver.cs
@@ -6,7 +6,7 @@ public static class ListSaver
 {
     public static void SaveList()
     {
-        using StreamWriter listWriter = File.CreateText("users/users.json");
+        using StreamWriter listWriter = File.CreateText($"/Users/piotrszymala/RiderProjects/SocketProgramming/Server/bin/Debug/net7.0/users/users.json");
         var result = JsonConvert.SerializeObject(ServerExecuter.Users);
         listWriter.Write(result);
     }
