82af67259fb910b8322b286886ef1356fae130ef
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-04-10 03:32:07 +0200

    Define obligatory properties in IUser.cs

--- Shared/IUser.cs
+++ Shared/IUser.cs
@@ -1,6 +1,12 @@
+using Shared.Models;
+
 namespace Shared;
 
 public interface IUser
 {
-    
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public Privileges Privileges { get; set; } 
+    public List<MessageToUser> Inbox { get; set; }
+
 }
