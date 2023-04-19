379d523cd6f12735d411266d3cbe65a6a30778dd
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-19 23:26:23 +0200

    Add Id property to IUser interface.

--- Shared/IUser.cs
+++ Shared/IUser.cs
@@ -4,6 +4,7 @@ namespace Shared;
 
 public interface IUser
 {
+    public int Id { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public Privileges Privileges { get; set; } 
