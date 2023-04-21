19781a5689affced52668c43eac8378524b2c158
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-21 23:55:12 +0200

    Add logger to check output.

--- SocketProgramming.Tests/LoggerImplementationTests.cs
+++ SocketProgramming.Tests/LoggerImplementationTests.cs
@@ -1,14 +1,22 @@
 using Shared;
+using Xunit.Abstractions;
 
 namespace SocketProgramming.Tests;
 
 public class LoggerImplementationTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public LoggerImplementationTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void LoggerTest()
     {
         Logger logger = new Logger();
-
+        _testOutputHelper.WriteLine(Environment.CurrentDirectory);
         try
         {
            // throw new Exception("Testing");
