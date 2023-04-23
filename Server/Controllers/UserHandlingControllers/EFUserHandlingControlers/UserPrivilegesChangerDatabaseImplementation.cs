3d081e8946c295934aad7e8a0e4d8709255d9c9a
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-23 22:18:28 +0200

    Write database system implementation of ChangePrivileges method in UserPrivilegesChangerDatabaseImplementation class.

--- Server/Controllers/UserHandlingControllers/EFUserHandlingControlers/UserPrivilegesChangerDatabaseImplementation.cs
+++ Server/Controllers/UserHandlingControllers/EFUserHandlingControlers/UserPrivilegesChangerDatabaseImplementation.cs
@@ -1,11 +1,47 @@
 using Server.Interfaces;
+using Shared;
+using Shared.Entities;
+using Shared.Models;
 
 namespace Server.Controllers.UserHandlingControllers.EFUserHandlingControlers;
 
 public class UserPrivilegesChangerDatabaseImplementation : IUserPrivilegesChanger
 {
+    private MyBoardsContext _myBoardsContext;
+    private IDataSender _dataSender;
+    private IDataReceiver _dataReceiver;
+    private ITransferStructure _transferStructure;
+
+    public UserPrivilegesChangerDatabaseImplementation(MyBoardsContext myBoardsContext, IDataSender dataSender, IDataReceiver dataReceiver, ITransferStructure transferStructure)
+    {
+        _myBoardsContext = myBoardsContext;
+        _dataSender = dataSender;
+        _dataReceiver = dataReceiver;
+        _transferStructure = transferStructure;
+    }
+
     public void ChangePrivileges()
     {
+        var message = _dataSender.SendData("Which user's privileges you want to change?");
+        _transferStructure.Send(message);
         
+        var username = _dataReceiver.GetData();
+
+        var userToChangePrivileges = _myBoardsContext.Users.FirstOrDefault(u => u.Username.Equals(username));
+
+        if (_myBoardsContext.Users.Contains(userToChangePrivileges))
+        {
+            userToChangePrivileges.Privileges = userToChangePrivileges.Privileges == Privileges.Admin ? Privileges.User : Privileges.Admin;
+
+            _myBoardsContext.SaveChanges();
+            
+            message = _dataSender.SendData($"Privileges for {userToChangePrivileges.Username} changed.");
+            _transferStructure.Send(message);
+        }
+        else
+        {
+            message = _dataSender.SendData("User does not exist.");
+            _transferStructure.Send(message);
+        }
     }
 }
